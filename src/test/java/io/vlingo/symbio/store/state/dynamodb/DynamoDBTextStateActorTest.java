6f522ed0af2248e5e6722fe00200c81216e8be40
Vaughn Vernon <vaughn@forcomprehension.com>
2019-01-03 16:52:23 -0700

    Test with common implementation.

--- src/test/java/io/vlingo/symbio/store/state/dynamodb/DynamoDBTextStateActorTest.java
+++ src/test/java/io/vlingo/symbio/store/state/dynamodb/DynamoDBTextStateActorTest.java
@@ -10,68 +10,35 @@ package io.vlingo.symbio.store.state.dynamodb;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
-import java.util.UUID;
+import org.junit.Before;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.Protocols;
 import io.vlingo.actors.World;
-import io.vlingo.symbio.Metadata;
-import io.vlingo.symbio.State;
 import io.vlingo.symbio.State.TextState;
 import io.vlingo.symbio.store.state.Entity1;
 import io.vlingo.symbio.store.state.StateStore;
-import io.vlingo.symbio.store.state.TextStateStore;
+import io.vlingo.symbio.store.state.Entity1.Entity1TextStateAdapter;
 import io.vlingo.symbio.store.state.dynamodb.adapters.RecordAdapter;
 import io.vlingo.symbio.store.state.dynamodb.adapters.TextStateRecordAdapter;
 import io.vlingo.symbio.store.state.dynamodb.interests.CreateTableInterest;
 
-public class DynamoDBTextStateActorTest extends DynamoDBStateActorTest<TextStateStore, TextState> {
+public class DynamoDBTextStateActorTest extends DynamoDBStateActorTest<TextState> {
+    @Before
+    public void setUp() {
+      super.setUp();
+      final Entity1TextStateAdapter adapter = new Entity1TextStateAdapter();
+      stateStore.registerAdapter(Entity1.class, adapter);
+      adapterAssistant.registerAdapter(Entity1.class, adapter);
+    }
+
     @Override
     protected Protocols stateStoreProtocols(World world, StateStore.Dispatcher dispatcher, AmazonDynamoDBAsync dynamodb, CreateTableInterest interest) {
         return world.actorFor(
-                Definition.has(DynamoDBTextStateActor.class, Definition.parameters(dispatcher, dynamodb, interest)),
-                new Class[]{TextStateStore.class, StateStore.DispatcherControl.class}
-        );
-    }
-
-    @Override
-    protected void doWrite(TextStateStore actor, TextState state, StateStore.WriteResultInterest<TextState> interest) {
-        actor.write(state, interest);
-    }
-
-    @Override
-    protected void doRead(TextStateStore actor, String id, Class<?> type, StateStore.ReadResultInterest<TextState> interest) {
-        actor.read(id, type, interest);
-    }
-
-    @Override
-    protected TextState nullState() {
-        return TextState.Null;
-    }
-
-    @Override
-    protected TextState randomState() {
-        return new State.TextState(
-                UUID.randomUUID().toString(),
-                Entity1.class,
-                1,
-                UUID.randomUUID().toString(),
-                1,
-                new Metadata(UUID.randomUUID().toString(), UUID.randomUUID().toString())
-        );
-    }
-
-    @Override
-    protected TextState newFor(TextState oldState) {
-        return new State.TextState(
-                oldState.id,
-                oldState.typed(),
-                oldState.typeVersion,
-                oldState.data,
-                oldState.dataVersion + 1,
-                oldState.metadata
+                Definition.has(DynamoDBStateActor.class, Definition.parameters(dispatcher, dynamodb, interest, new TextStateRecordAdapter())),
+                new Class[]{StateStore.class, StateStore.DispatcherControl.class}
         );
     }
 
